175a4ce9b8b48c912f4a1b8394ff39c4e24204fe
shubhangi1031 <139439057+shubhangi1031@users.noreply.github.com>
2023-07-13 16:02:30 +0530

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,20 @@
+void main(){
+  int score = 23;
+  var count = 23;
+  int hexValue= 0xEADEBAEE;
+
+  double percentage = 93.4;
+  var percent = 82.33333;
+  double exponents = 1.42e5;
+
+  String name = "henry";
+  var company = "Google";
+
+  bool isValid = true;
+  var isAlive = false;
+
+  print(score);
+  print(isValid);
+  print(exponents);
+  print(hexValue);
+}
