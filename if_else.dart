6db4e346083a4cb1b3cd76bd2c7195519504e303
shubhangi1031 <139439057+shubhangi1031@users.noreply.github.com>
2023-07-13 16:06:15 +0530

    Create if_else.dart

---
+++ if_else.dart
@@ -0,0 +1,32 @@
+
+void main(){
+
+  var salary = 15000;
+
+  if(salary>20000)
+    {
+      print("promotion");
+    }
+  else
+    {
+      print("no promotion");
+    }
+
+  var marks = 70;
+  if(marks>=90 && marks<100)
+    {
+      print("A+ grade");
+    }
+  else if(marks >=80 && marks<90)
+    {
+      print("A grade");
+    }
+  else if(marks >=70 && marks<80)
+    {
+      print("B+ grade");
+    }
+
+  else {
+    print("invalid marks");
+  }
+}
