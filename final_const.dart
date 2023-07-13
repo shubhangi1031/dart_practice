9d1f90940ef3368a49f2c50e0f4e661e58c5acc0
shubhangi1031 <139439057+shubhangi1031@users.noreply.github.com>
2023-07-13 16:01:28 +0530

    Create final_const.dart

---
+++ final_const.dart
@@ -0,0 +1,17 @@
+void main(){
+  final cityName = "mumbai";
+  //cityName = "delhi";
+
+  final String countryName = "India";
+
+  const PI = 3.14;
+  const gravity = 9.8;
+
+}
+
+class Circle{
+  final color = 'red';
+  static const PI = 3.14;
+
+  //at class level, const can be defined with the static keyword
+}
