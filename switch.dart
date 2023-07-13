d1cd8fbb23ec32bbad491157b6012500fcdf81eb
shubhangi1031 <139439057+shubhangi1031@users.noreply.github.com>
2023-07-13 22:53:47 +0530

    Create switch.dart

---
+++ switch.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  //only applicable for int and String
+
+  String grade = 'A';
+
+  switch (grade)
+      {
+        case 'A' :
+          print("Excellent grade A");
+          break;
+        case 'B' :
+          print("Good grade B");
+          break;
+        case 'C' :
+          print("Average grade C");
+          break;
+        case 'D' :
+          print("Poor grade D");
+          break;
+    default:
+      print("You have failed");
+
+  }
+}
